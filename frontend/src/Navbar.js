3dc0a6f80ae71f5a64522f2dc05dbf014f6c3b0c
yaeli <yaelimaximov@gmail.com>
2024-08-28 16:26:40 +0300

    logo design

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // קובץ CSS נפרד לעיצוב
-import logo from './Design 7.png'; // עדכן את הנתיב לתמונה שלך
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
+import logo from './Design 7.png';
 
 function Navbar() {
+  const [isRolling, setIsRolling] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Activate the roll animation and typing animation on route change
+    setIsRolling(true);
+    const timer = setTimeout(() => {
+      setIsRolling(false);
+    }, 4000); // Adjust the duration according to the CSS animation
+
+    return () => clearTimeout(timer);
+  }, [location]);
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
         <li><Link to="/menu" className="navbar-link">תפריטים</Link></li>
         <li><Link to="/orderType" className="navbar-link">הזמנות אונליין</Link></li>
         
-        {/* לוגו באמצע */}
-        <li className="navbar-logo">
+        {/* Logo with rolling and typing animation */}
+        <li className={`navbar-logo ${isRolling ? 'roll' : ''}`}>
           <img src={logo} alt="Logo" className="logo-image" />
+          <span className="logo-text">קפה הפוך</span>
         </li>
 
         <li><Link to="/branch/1" className="navbar-link">סניפים</Link></li>
