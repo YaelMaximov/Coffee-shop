a2c957389be0551ba497270b022c4c4293c71f85
yaeli <yaelimaximov@gmail.com>
2024-08-28 12:40:00 +0300

    logo

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // קובץ CSS נפרד לעיצוב
+import logo from './Design 7.png'; // עדכן את הנתיב לתמונה שלך
 
 function Navbar() {
   return (
@@ -8,6 +9,12 @@ function Navbar() {
       <ul className="navbar-list">
         <li><Link to="/menu" className="navbar-link">תפריטים</Link></li>
         <li><Link to="/orderType" className="navbar-link">הזמנות אונליין</Link></li>
+        
+        {/* לוגו באמצע */}
+        <li className="navbar-logo">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </li>
+
         <li><Link to="/branch/1" className="navbar-link">סניפים</Link></li>
         <li><Link to="/login" className="navbar-link">התחברות</Link></li>
       </ul>
