9caf6580a618e89ad130ae618c1d51a78cbf6c7f
yaeli <yaelimaximov@gmail.com>
2024-09-09 13:15:48 +0300

    design for phone in order page

--- frontend/src/Home/OrderPage.js
+++ frontend/src/Home/OrderPage.js
@@ -1,12 +1,10 @@
-
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMenu } from '../Providers/MenuProvider';
 import { OrderContext } from '../Providers/OrderProvider'; // Import OrderContext
 import './css/OrderPage.css';
 import DishPopup from './DishPopup';
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />;
 
 export default function OrderPage() {
   const navigate = useNavigate();
@@ -16,20 +14,21 @@ export default function OrderPage() {
   const [selectedDish, setSelectedDish] = useState(null);
   const [editIndex, setEditIndex] = useState(null);
   const { order, updateOrder } = useContext(OrderContext); // Use OrderContext
-  //const { items: cart } = order;
   const cart = order?.items || [];
-
 
   const handlePayment = () => {
     updateOrder({ ...order, items: cart, total: getTotalPrice() }); // Update OrderContext
     navigate('/payment');
   };
+
   const categories = [...new Set(menu.map((dish) => dish.category))];
+
   const filteredMenu = menu.filter(
     (dish) =>
       dish.category === selectedCategory &&
       dish.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
   const addToCart = (orderItem) => {
     if (editIndex !== null) {
       updateOrder({
@@ -75,6 +74,7 @@ export default function OrderPage() {
       ),
     });
   };
+
   const handleResetOrder = () => {
     updateOrder({
       ...order,
@@ -85,9 +85,11 @@ export default function OrderPage() {
   return (
     <div className="order-page">
       <div className="header-container">
-      {/* <img src="https://imageproxy.wolt.com/venue/61eea7232d54f85baeb99f9d/798f6406-81ea-11ec-a506-f20b69867cbd_yaelitz.jpg" alt="תיאור התמונה" className="header-image" /> */}
-
-        <img src="https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/255245272_4902502489769622_2324489355499356995_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=EAvoMnTGp0gQ7kNvgH_vfTF&_nc_ht=scontent.ftlv1-1.fna&oh=00_AYCTVC6T1vVujJuneON1u-MiUcCxWqmXYYRSz-DLVWFT7w&oe=66DDF9CE" alt="תיאור התמונה" className="header-image" />
+        <img
+          src="https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/255245272_4902502489769622_2324489355499356995_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=EAvoMnTGp0gQ7kNvgH_vfTF&_nc_ht=scontent.ftlv1-1.fna&oh=00_AYCTVC6T1vVujJuneON1u-MiUcCxWqmXYYRSz-DLVWFT7w&oe=66DDF9CE"
+          alt="תיאור התמונה"
+          className="header-image"
+        />
       </div>
 
       <div className="menu-header">
@@ -102,7 +104,7 @@ export default function OrderPage() {
       </div>
 
       <div className="content-wrapper">
-      <div className="category-navigation">
+        <div className="category-navigation">
           <h3>קטגוריות</h3>
           <ul>
             {categories.map((category) => (
@@ -116,6 +118,7 @@ export default function OrderPage() {
             ))}
           </ul>
         </div>
+
         <div className="menu-list">
           {filteredMenu.map((dish) => (
             <div className="menu-item" key={dish.dish_id}>
@@ -124,31 +127,45 @@ export default function OrderPage() {
                   <h3>{dish.name}</h3>
                   <p>{dish.description}</p>
                   <p>₪{dish.price}</p>
-                  <button className="add-to-cart" onClick={() => setSelectedDish(dish)}>הוסף לסל</button>
+                  <button className="add-to-cart" onClick={() => setSelectedDish(dish)}>
+                    הוסף לסל
+                  </button>
                 </div>
                 <img src={dish.image_url} alt={dish.name} className="dish-image-order" />
               </div>
             </div>
           ))}
         </div>
+
         <div className="cart-summary">
           <h3>סיכום הזמנה</h3>
           {cart.length === 0 ? (
             <>
-              {/* <img className="basket" src="https://cdn-icons-png.freepik.com/512/7877/7877599.png" alt="Cart" width={200} /> */}
-              <img className="basket" src="https://cdn-icons-png.flaticon.com/512/3721/3721650.png" alt="Cart" width={200} />
+              <img
+                className="basket"
+                src="https://cdn-icons-png.flaticon.com/512/3721/3721650.png"
+                alt="Cart"
+                width={200}
+              />
               <p>הסל שלך ריק</p>
             </>
           ) : (
             <div className="cart-items">
-              <button className="reset-order" onClick={handleResetOrder}>אפס הזמנה</button>
+              <button className="reset-order" onClick={handleResetOrder}>
+                אפס הזמנה
+              </button>
               {cart.map((item, index) => (
                 <div key={index} className="cart-item">
                   <h4>{item.dish}</h4>
                   <p>כמות: {item.quantity}</p>
-                  <p>תוספות: {Object.entries(item.extras).flatMap(([category, extras]) => 
-                    extras.map(extra => extra.name)
-                  ).join(', ')}</p>
+                  <p>
+                    תוספות:{' '}
+                    {Object.entries(item.extras)
+                      .flatMap(([category, extras]) =>
+                        extras.map((extra) => extra.name)
+                      )
+                      .join(', ')}
+                  </p>
                   <p>מחיר: ₪{item.totalPrice}</p>
                   <div className="cart-item-actions">
                     <button onClick={() => handleEditItem(index)}>ערוך</button>
@@ -165,10 +182,14 @@ export default function OrderPage() {
               <p className="total-price">סה"כ: ₪{getTotalPrice().toFixed(2)}</p>
             </div>
           )}
-          <button className="checkout-button" disabled={cart.length === 0} onClick={handlePayment}>לתשלום</button>
+          <button
+            className="checkout-button"
+            disabled={cart.length === 0}
+            onClick={handlePayment}
+          >
+            לתשלום
+          </button>
         </div>
-
-        
       </div>
 
       {selectedDish && (
